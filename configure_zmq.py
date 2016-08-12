9a2bc0b5c3233e5b756bd27083245c5de26f85cc
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-08-12 17:05:52 +0300

    fix import

--- configure_zmq.py
+++ configure_zmq.py
@@ -140,6 +140,7 @@ def start_proxy_on_nodes(nodes):
         print get_managable_ip_from_node(node)
         if not args.dry_run:
             print get_command_output('scp generate_proxy_config.py %s:/tmp' % node)
+            print get_command_output('scp hack_config_with_zmq.py %s:/tmp' % node)
             print get_command_output("ssh %s 'python /tmp/generate_proxy_config.py'" % node)
 
             print ("ssh %s 'nohup oslo-messaging-zmq-proxy --debug True "
