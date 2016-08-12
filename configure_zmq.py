453a2ffc0d0a0a383143387edac3dc58766e483a
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-08-12 15:28:03 +0300

    Don't hang remote ssh

--- configure_zmq.py
+++ configure_zmq.py
@@ -173,7 +173,7 @@ def start_proxy_on_nodes_venv(nodes):
             print get_command_output('scp zmq-proxy.conf %s:/etc' % node)
 
             print get_command_output("ssh %s 'rm -rf /tmp/venv /tmp/oslo.messaging && git clone https://git.openstack.org/openstack/oslo.messaging /tmp/oslo.messaging && cd /tmp/oslo.messaging && git fetch https://git.openstack.org/openstack/oslo.messaging refs/changes/79/352579/14 && git checkout FETCH_HEAD'" % node)
-            print get_command_output("ssh %s 'mkdir /tmp/venv && cd /tmp/venv && virtualenv --no-setuptools . && "
+            print get_command_output("ssh -f %s 'mkdir /tmp/venv && cd /tmp/venv && virtualenv --no-setuptools . && "
                                      ". /tmp/venv/bin/activate && "
                                      "pip install setuptools && "
                                      "pip install eventlet PyYAML oslo.messaging petname redis zmq && "
