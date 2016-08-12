edccb7bb4e28bc934200adabb62ae2a2ae50bd24
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-08-12 13:28:04 +0300

    fixed node access by ssh

--- configure_zmq.py
+++ configure_zmq.py
@@ -164,7 +164,7 @@ def install_oslo_messaging_package(package_url, package_name, nodes):
     for node in nodes:
         print '\nInstalling %s on %s' % (package_url, node)
         if not args.dry_run:
-            print get_command_output("ssh %s 'mkdir /tmp/zmq-package/'")
+            print get_command_output("ssh %s 'mkdir /tmp/zmq-package/'" % node)
             print get_command_output("ssh %s 'wget -N -O /tmp/zmq-package/%s "
                                      "%s'" % (node, package_name, package_url))
             print get_command_output("ssh %s 'dpkg -i /tmp/zmq-package/%s'" % (node, package_name))
