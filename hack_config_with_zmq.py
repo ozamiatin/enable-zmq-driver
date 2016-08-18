43f6e85ffac598e5dc6b5ef1f7ed5cd0eaa5dc77
ozamiatin <ozamiatin@mirantis.com>
2016-08-18 15:38:19 +0300

    Fixed file name arg

--- hack_config_with_zmq.py
+++ hack_config_with_zmq.py
@@ -63,7 +63,6 @@ def main():
             newcontent.append('debug = True\n')
             newcontent.append('default_log_levels=amqp=WARN,amqplib=WARN,boto=WARN,iso8601=WARN,keystonemiddleware=WARN,oslo.messaging=DEBUG,oslo_messaging=DEBUG,qpid=WARN,requests.packages.urllib3.connectionpool=WARN,requests.packages.urllib3.util.retry=WARN,routes.middleware=WARN,sqlalchemy=WARN,stevedore=WARN,suds=INFO,taskflow=WARN,urllib3.connectionpool=WARN,urllib3.util.retry=WARN,websocket=WARN\n')
             newcontent.append('rpc_backend = zmq\n')
-            #newcontent.append('transport_url = zmq+redis://%s:6379\n' % REDIS_HOST)
 
         if RPC_BACKEND.match(line) or REDIS_SECTION.match(line) or ZMQ_SECTION.match(line):
             continue
@@ -80,7 +79,7 @@ def main():
     newcontent.append('[matchmaker_redis]')
     newcontent.append('host=%s' % REDIS_HOST)
 
-    with open(sys.argv[1], 'w') as fl:
+    with open(sys.argv[2], 'w') as fl:
         fl.write(''.join(newcontent))
 
 
